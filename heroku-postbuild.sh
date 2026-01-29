e039e2e67f857ac3dfcecd6543a186ef6a9216c0
Felipe de Andrade <fdeandrade@fdeandr-ltm8ymq.internal.salesforce.com>
2026-01-29 14:54:23 -0800

    Initial commit

--- heroku-postbuild.sh
+++ heroku-postbuild.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 # This script runs after npm install on Heroku
-# It removes large files that are not needed in the production slug
-# Note: Files are removed AFTER build to reduce slug size
+# It removes build cache to reduce slug size
+# Note: Images are now compressed (~93MB total) and kept in the slug
 
-echo "Cleaning up large files to reduce Heroku slug size..."
+echo "Cleaning up build cache to reduce Heroku slug size..."
 
-# Remove large product images (774MB) - these should be served from CDN in production
-if [ -d "public/images/products" ]; then
-  echo "Removing public/images/products/ (774MB)..."
-  rm -rf public/images/products/
-fi
-
-# Remove large support resources (132MB) - these should be served from CDN
-if [ -d "public/resources" ]; then
-  echo "Removing public/resources/ (132MB)..."
-  rm -rf public/resources/
-fi
-
-# Remove build cache if it exists
+# Remove build cache if it exists (this can be large)
 if [ -d ".next/cache" ]; then
   echo "Removing .next/cache/..."
   rm -rf .next/cache/
 fi
 
-echo "Cleanup complete. Slug size should be significantly reduced."
+# Images are now compressed and kept in the slug (~93MB total)
+# This is well within the 500MB limit
+echo "Cleanup complete. Images are kept in the slug (compressed)."
